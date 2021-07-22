743c73594e313056b0787ad629cfdc989ec0cc3c
pikachom <pikachom@gmail.com>
2021-07-23 02:43:36 +0900

    login 개발 완료, #2.5 social login 시작할 차례

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, {useEffect, useState} from "react";
+import AppRouter from "components/Router";
+import {authService} from "fbase"
 
 function App() {
-    return <AppRouter />;
-}
+    const [init, setInit] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        authService.onAuthStateChanged((user) => {
+            if(user){
+                setIsLoggedIn(true);
+            } else {
+                setIsLoggedIn(false);
+            }
+            setInit(true);
+            console.log(user)
+        });
+    }, []);
+    return (
+        <>
+            {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+            <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+        </>
+
+    );
+};
 
 export default App;
